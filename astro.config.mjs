ce970180302a5b35519f4f20ada909a0d90948de
shouta0715 <Myheroes0715@gmail.com>
2023-09-05 20:14:14 +0900

    add library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+/* eslint-disable import/no-unresolved */
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [tailwind(), react()],
+});
